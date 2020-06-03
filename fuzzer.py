ee5e5ccab395aa7e9615b126f68c5197141ee976
kooroshrz <knasa.kr@aut.ac.ir>
2020-06-03 18:53:31 +0430

    config json file for input

--- fuzzer.py
+++ fuzzer.py
@@ -4,103 +4,131 @@ import threading
 import random
 import string
 from optparse import OptionParser
+import argparse
 import json
 
 session = requests.Session()
+
 payloads_list = []
 paylosds_size = 0
 
-
-# command line options
+# config options
 input_file = ''
 output_file = ''
 headers = ''
 data = ''
 method = ''
 url = ''
-threads = 3 # default value
+threads = 0
 
+config_file = 'fuzz-web-with-me\\config.json'
 
-
-'''
+#for debug purpose
 proxies = {
     'http' : '127.0.0.1',
     'https' : '127.0.0.1:8080'
 }
-'''
+
 
 def distribute_payloads():
 
     threads_list = []
     global threads
+    global method
 
     for t in range(threads):
 
         start_offset = t * (paylosds_size//threads)
         end_offset = (t+1) * (paylosds_size//threads)
 
-        thread = threading.Thread(target=send_requests, args=(start_offset, end_offset))
+        thread = threading.Thread(target=send_requests, args=(method, url, start_offset, end_offset, headers, data))
 
         threads_list.append(thread)
         thread.start()
 
 
-def send_requests(start_offset, end_offset):
+def send_requests(method, url, start_offset, end_offset, headers=None, data=None):
+
+    if method == 'GET':
+
+        for i in range(start_offset, end_offset):
+
+            payload = payloads_list[i][:-1]
+
+            url = '{}'.format(payload)
+
+            response = session.get(url)
+
+            if response.status_code == 200:
+                print("found order with order code : {}".format(payload))
+
+                with open(output_file, 'a') as f1:
+                    f1.write(response.text)
+                    f1.close()
 
 
-    for i in range(start_offset, end_offset):
+    elif method == 'POST':
 
-        payload = payloads_list[i][:-1]
+        for i in range(start_offset, end_offset):
 
-        url = '{}'.format(payload)
-        response = session.get(url)
+            payload = payloads_list[i][:-1]
 
-        if response.status_code == 200:
-            print("found order with order code : {}".format(payload))
+            data_temp = json.loads(json.dumps(data).replace('FUZZ', payload))
+        
+            try:
+                response = session.post(url, data=data_temp, headers=headers)
+            except OSError as err:
+                print(err)
 
-            with open(output_file, 'a') as f1:
-                f1.write(response.text)
-                f1.close()
+            result = json.loads(response.text)
 
-
+            if result['isRegistered'] == True:
+                with open(output_file, 'a') as fo:
+                    fo.write("0912" + payload + json.dumps(result) + "\n")
+                    fo.close()
+                
 
 def initialize():
 
-    global session
-
+    global config_file
+    global input_file
     global payloads_list
-    payloads_list = open(input_file, 'r').readlines()
-
     global paylosds_size
+    global headers
+    global data
+    
+    payloads_list = open(input_file, 'r').readlines()
     paylosds_size = len(payloads_list)
+
+    with open(config_file, 'r') as f:
+
+        config = json.loads(f.read())
+
+        headers = config['headers']
+        data = config['data']
 
 
 def parse_command():
-    parser = OptionParser()
-
-    parser.add_option("-i", dest="payloads",  help="read payloads from input file")
-
-    parser.add_option("-o", dest="output", help="write result to output file")
-
-    parser.add_option("-u", dest="url", help="url of target website")
-
-    parser.add_option("-d", dest="data", help="data to send with POST method")
-
-    parser.add_option("-m", dest="method", help="http method")
-
-    parser.add_option("-H", dest="header", help="specific http header")
-
-    parser.add_option("-t", dest="threads", help="number of threads")
-
-    (options, args) = parser.parse_args()
     
-    print(options)
+    global config_file
 
 
+    global input_file
+    global output_file
+    global threads
+    global method
+    global url
+
+    result = json.loads(open(config_file, 'r').read())
+    input_file = result['input_file']
+    output_file = result['output_file']
+    threads = int(result['threads'])
+    url = result['url']
+    method = result['method']
 
 
 if __name__ == "__main__":
     
     parse_command()
-    # initialize()
-    # distribute_payloads()
+    initialize()
+    distribute_payloads()
